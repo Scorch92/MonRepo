6220700d036eda5ae3a6701c08c049e5a6c809f4
Gautier Julien <scorch92@hotmail.be>
2015-10-16 14:30:54 +0200

    Ajout fonction bizz

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		bizz(6);
+	}
+	
+	public static void bizz(int n)
+	{
+		if(n % 3 == 0)
+		{
+			System.out.println("BIZZ");
+		}
+	}
+}
